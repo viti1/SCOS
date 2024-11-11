f228f033e0a6661d079efd72ab1e9ddee1229a18
Vika Lab PC <viti.tr@gmail.com>
2024-11-11 15:18:07 +0200

    plots of shaareiZedek

--- forReports/ShaareiZedek_Plot.m
+++ forReports/ShaareiZedek_Plot.m
@@ -1,51 +1,95 @@
-recName = ['C:\Users\' getenv('username') '\OneDrive - Bar Ilan University\SCOS_Records\ShaareiZedek\B4 21.07.2024'];
-SCOSdir = dir([recName '\Main*']);
-SCOSdir2 = dir([recName filesep SCOSdir.name , '\LocalStd*_corr.mat']);
-% SCOSfile = fullfile(recName,SCOSdir(1).name, SCOSdir2(1).name );
+clear
 
-[recFolder , recRawName ]= fileparts(recName);
+recName = ['C:\Users\' getenv('username') '\OneDrive - Bar-Ilan University - Students\PhD Research\SCOS\Records\ShaareiZedek__\C4'];
+% recName = 'C:\Users\tarlevi\OneDrive - Bar Ilan University\SCOS_Records\ShaareiZedek\B3 21.07.2024';
+[recordsFolder , recRawName ]= fileparts(recName);
 participantID = recRawName(1:2);
-NIRSFolder = [ recFolder '\NIRS Data'];
-nirsFile   = dir([NIRSFolder '\' participantID '*.csv']); nirsFile = fullfile(NIRSFolder,nirsFile.name);
-timingFile = dir([NIRSFolder '\' participantID '*timing info.txt']); timingFile = fullfile(NIRSFolder,timingFile.name);
-firstFrameDir = dir([recName,filesep,SCOSdir(1).name '\*_0001.tiff']);
-tmp = strsplit(firstFrameDir(1).date);
-startTimeSCOS = tmp{2};
 
-NIRS = readtable(nirsFile);
-NIRS = renamevars(NIRS,2:7,{'Date','Time','Left','Right','SedlineEvents','Remarks'});
-if ischar(NIRS.Left)
-    NIRS.Left = str2double(NIRS.Left);
-    NIRS.Right = str2double(NIRS.Right);
+%% Timing
+timingFileDir = dir([recName '\' participantID '*timing info.txt']); 
+if isempty(timingFileDir)
+    timingFileDir = dir([recordsFolder '\NIRS Data\' participantID '*timing info.txt']); 
+    if isempty(timingFileDir)
+        error('Could not find Timing file');
+    end
+    timingFile = fullfile(recordsFolder,'NIRS Data',timingFileDir.name);
+else
+    timingFile = fullfile(recName,timingFileDir.name);
 end
-% firstIdx = find(~isnan(NIRS.Right) | ~isnan(NIRS.Left),1);
-% startTimeNIRS = NIRS.Time(1);
-% NIRStoSCOStimeDiff = etime(datevec(char(startTimeNIRS)),datevec(startTimeSCOS))/60; % Nirs - scos in min
-
-% startTimeNIRS = NIRS.Time(firstIdx);
 Timing = readtable(timingFile);
-startTime = Timing.Time(1);
+baselineTime = Timing.Time(1);
+%% NIRS
+NIRSFolder = [ recordsFolder '\NIRS Data'];
+nirsFile   = dir([NIRSFolder '\' participantID '*.csv']);
+if ~isempty(nirsFile)    
+    nirsFile = fullfile(NIRSFolder,nirsFile.name);
+    NIRS = readtable(nirsFile);
+    NIRS = renamevars(NIRS,2:7,{'Date','Time','Left','Right','SedlineEvents','Remarks'});
+    if ischar(NIRS.Left)
+        NIRS.Left = str2double(NIRS.Left);
+        NIRS.Right = str2double(NIRS.Right);
+    end
+end
+%% SCOS
+SCOSdir = dir([recName '\Main*']);
+if isempty(SCOSdir)
+    SCOS_mat_dir = dir([recName , '\LocalStd*_corr.mat']);
+    if isempty(SCOS_mat_dir)
+        error(['Could not find ' recName , '\LocalStd*_corr.mat file']);
+    end
+    SCOSfile = fullfile(recName, SCOS_mat_dir(1).name );
+    SCOS = load(SCOSfile);
+    if ~exist('startTimeSCOS','var')
+        tmp = strsplit(SCOS.startDateTime);
+        startTimeSCOS = tmp{2};
+    end
+else
+    SCOS_mat_dir = dir([recName filesep SCOSdir(1).name , '\LocalStd*_corr.mat']);
+    SCOSfile = fullfile(recName,SCOSdir(1).name, SCOS_mat_dir(1).name );
+    firstFrameDir = dir([recName,filesep,SCOSdir(1).name '\*_0001.tiff']);
+    tmp = strsplit(firstFrameDir.date);
+    startTimeSCOS = tmp{2};
+    SCOS = load(SCOSfile);
+end
 
-%% Plot
-f = figure('Units','Normalized','Position',[0.05 0.1 0.9 0.8]);
-% SCOS = load();
-% figure('Units','Normalized','Position',[0.05 0.05 0.8 0.8]);
-% subplot(3,1,1);
-% plot(timeVec/60,1./corrSpeckleContrast_jumpsCorrected{1}); hold on;
-% xlabel('[min]');
-% ylabel('1/K^2');
-% markTiming(timingFile,startTimeSCOS)
+%% Plot 
+if isempty(nirsFile); nPlots = 2; else; nPlots =3; end
+fig1 = figure('Units','Normalized','Position',[0.05 0.1 0.9 0.8]);
+BFi = 1./SCOS.corrSpeckleContrast{1};
+BFi(SCOS.corrSpeckleContrast{1} < 0 ) = NaN;
+BFi(SCOS.meanVec{1} < 4 ) = NaN;
+frameRate = SCOS.info.name.FR;
+secForNormalization = 10;
+% rBFi = BFi/prctile(BFi(1:round(10*frameRate)),5); % normalize by 5% percentile in first 10 sec
+rBFi = BFi/mean(BFi(5:round(secForNormalization*frameRate)),'omitnan'); % normalize by secForNormalization
 
-% subplot(3,1,2);
-% plot(timeVec/60,meanVec{1}); hold on;
-% xlabel('[min]');
-% ylabel('Intensity');
-% markTiming(timingFile);
+subplot(nPlots,1,1);
+SCOStimeVec =  SCOS.timeVec/60 - minutes(duration(baselineTime,'InputFormat','hh:mm') - duration(startTimeSCOS));
+plot(SCOStimeVec,rBFi); hold on;
+xlabel('[min]');
+ylabel('rBFI');
+ylim([0 4]);
+grid on; grid minor
+markTiming(timingFile)
 
-subplot(3,1,3);
-NIRStimeVec = minutes(NIRS.Time -  duration(startTime,'InputFormat','hh:mm'));
-h(1) = plot(NIRStimeVec,NIRS.Left,'color',[0 0.4 0],'DisplayName','Left'); hold on;
-h(2) = plot(NIRStimeVec,NIRS.Right,'color',[0.5 0.8 0.2],'DisplayName','Right'); hold on;
+subplot(nPlots,1,2);
+plot(SCOStimeVec,SCOS.meanVec{1}); hold on;
+xlabel('[min]');
+ylabel('Intensity');
+grid on; grid minor
 markTiming(timingFile);
-legend(h); % TBD check which is left and which is right
 
+if ~isempty(nirsFile)
+    subplot(3,1,3);
+    NIRStimeVec = minutes(NIRS.Time -  duration(baselineTime,'InputFormat','hh:mm'));
+    % colors = {[0 0.4 0],[0.5 0.8 0.2]}; % green
+    colors = {[0.8 0 0.2],[0.5 0 0]}; % red
+    h(1) = plot(NIRStimeVec,NIRS.Left,'color',colors{1},'DisplayName','Left'); hold on;
+    h(2) = plot(NIRStimeVec,NIRS.Right,'color',colors{2},'DisplayName','Right'); hold on;
+    grid on; grid minor
+    markTiming(timingFile);
+    legend(h); % TBD check which is left and which is right
+    xlabel('[min]');
+    ylabel('Hb Saturation');
+end
+savefig(fig1, [ recName '\SignalsPlot.fig']);
